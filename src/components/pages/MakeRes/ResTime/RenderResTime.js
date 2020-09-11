6eb250b6301d4d5db0a0206a50086433fe2a46d6
Gregory Hawman <g.hawman@gmail.com>
2020-09-11 16:44:19 -0700

    added styling to the radio in the make a res component so that it looks like the figma doc

--- src/components/pages/MakeRes/ResTime/RenderResTime.js
+++ src/components/pages/MakeRes/ResTime/RenderResTime.js
@@ -1,34 +1,45 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
-import { Radio, Button } from 'antd';
+import { Layout, Menu, Radio, Button } from 'antd';
 
+const { Header, Content, Footer } = Layout;
 function ResTime(props) {
   const { onChange, radioStyle, value } = props;
 
   return (
-    <div>
-      <h>How much time will you need at the Community Co-Working space?</h>
-      <div>
-        <Radio.Group onChange={onChange} value={value}>
-          <Radio style={radioStyle} value={1}>
-            1-2 Hours
-          </Radio>
-          <Radio style={radioStyle} value={2}>
-            3-4 Hours
-          </Radio>
-          <Radio style={radioStyle} value={3}>
-            6 Hours
-          </Radio>
-        </Radio.Group>
-      </div>
-
-      <div>
-        <Button>
-          <Link to="/make-res">Next</Link>
-        </Button>
-      </div>
-    </div>
+    <Layout className="layout" style={{ background: 'white' }}>
+      <Content
+        style={{
+          padding: '50px 50px',
+          margin: '5% auto',
+          border: '1px solid lightgrey',
+          background: 'white',
+        }}
+      >
+        <h1>How long are you planning on staying?</h1>
+        <Content style={{ padding: '50px 150px', margin: 'auto auto' }}>
+          <div>
+            <Radio.Group onChange={onChange} value={value}>
+              <Radio style={radioStyle} value={1}>
+                1 - 2 Hours
+              </Radio>
+              <Radio style={radioStyle} value={2}>
+                3 - 4 Hours
+              </Radio>
+              <Radio style={radioStyle} value={3}>
+                6 Hours
+              </Radio>
+            </Radio.Group>
+          </div>
+          <div style={{ padding: '50px 10px' }}>
+            <Button>
+              <Link to="/make-res">Next</Link>
+            </Button>
+          </div>
+        </Content>
+      </Content>
+    </Layout>
   );
 }
 
