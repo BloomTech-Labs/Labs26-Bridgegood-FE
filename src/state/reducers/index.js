89c17237d4931418eb9ea8c67ca9a92bb34b9594
Gregory Hawman <g.hawman@gmail.com>
2020-09-17 09:35:47 -0700

    boilerplate actions, and reducers for redux

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,4 +1,19 @@
-// import all of your reducers into this file, and export them back out. 
+// import all of your reducers into this file, and export them back out.
 // This allows for the simplification of flow when importing reducers into your actions throughout your app.
 
+import {} from '../actions/index';
 
+const initialState = {
+  data: {
+    TimeAmount: 1,
+    Date: '1-1-20',
+    RoomType: 'Front',
+    TimeSlot: '10:00am',
+  },
+  isFetchingData: false,
+  error: '',
+};
+
+export const Reducer = (state = initialState, action) => {
+  return state;
+};
