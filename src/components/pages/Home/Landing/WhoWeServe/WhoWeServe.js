48ed57226ce675173e4a00bfb97c9ec86998f184
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2020-09-17 11:34:44 -0700

    who we serve section

---
+++ src/components/pages/Home/Landing/WhoWeServe/WhoWeServe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './WhoWeServe.module.css';
+
+const WhoWeServer = props => {
+  return (
+    <section className={classes.WhoWeServe}>
+      <h1>who we serve</h1>
+    </section>
+  );
+};
+
+export default WhoWeServer;
