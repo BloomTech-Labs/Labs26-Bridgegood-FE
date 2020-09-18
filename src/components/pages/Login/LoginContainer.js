165dbbb8245e5a02e9a66a9486e3a4924cff20ea
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2020-09-17 22:31:31 -0700

    added styles for login

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -3,10 +3,11 @@ import OktaSignIn from '@okta/okta-signin-widget';
 import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
 import { config } from '../../../utils/oktaConfig';
 
-import Header from '../../common/Header.js';
-import './LoginContainer.less';
+import Header from '../../pages/Home/Landing/Header/index.js';
+import Footer from '../../pages/Home/Landing/Footer/Footer.js';
 
 const LoginContainer = () => {
+  console.log('Footer');
   useEffect(() => {
     const { pkce, issuer, clientId, redirectUri, scopes } = config;
     // destructure your config so that you can pass it into the required fields in your widget.
@@ -56,6 +57,7 @@ const LoginContainer = () => {
       <section className="main-container">
         <div className="welcome-text"></div>
         <div id="sign-in-widget" />
+        <Footer />
       </section>
     </div>
   );
