d306f0b241b709c10af4c0a1bf53ad875f95878b
Yasir Hassan <hasanyasir01@gmail.com>
2020-09-20 23:00:13 -0700

    mission section

--- src/components/pages/Home/Landing/Mission/Information/Information.js
+++ src/components/pages/Home/Landing/Mission/Information/Information.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import { Button } from 'antd';
 import classes from './Information.module.css';
+import ImgIcon from '../../../../../../assets/images/Polygon.png';
 
 const Information = props => {
   return (
     <section className={classes.Information}>
-      <div className={classes.ourMission}>
-        <section className={classes.Tob}>
-          <h2>our mission</h2>
-        </section>
-
-        <section className={classes.Bottom}>
-          <h1>
-            Oakland’s FIRST tech-for-good, community-access co-working space and
-            education center
-          </h1>
-        </section>
+      <div className={classes.Left}>
+        <p>OUR MISSION</p>
+        <h1>
+          Oakland’s FIRST tech-for-good, community-access co-working space and
+          education center
+        </h1>
       </div>
-
-      <div className={classes.payForward}>
-        <section className={classes.Tob}>
-          <p>
-            Starting a career in design can be intimidating without a strong,
-            supportive network. The BRIDGEGOOD Creative Studio brings together
-            students of diverse background with common passion in design.
-          </p>
-          <p>
-            Nobody will be turned away because of financial restraints. That’s
-            why we are counting on your support to help make a lasting
-            difference for the students in the Bay Area.
-          </p>
-        </section>
-        <section className={classes.Bottom}>
-          <Button>pay it forward</Button>
-          <p>HOW WE ARE HELPING IMPROVE DESIGN INCLUSION</p>
+      <div className={classes.Right}>
+        <p>
+          Starting a career in design can be intimidating without a strong,
+          supportive network. The BRIDGEGOOD Creative Studio brings together
+          students of diverse background with common passion in design.
+        </p>
+        <p>
+          Nobody will be turned away because of financial restraints. That’s why
+          we are counting on your support to help make a lasting difference for
+          the students in the Bay Area.
+        </p>
+        <section>
+          <button>Pay It Forward</button>
+          <span>HOW WE ARE HELPING IMPROVE DESIGN INCLUSION</span>{' '}
+          <span>
+            <img src={ImgIcon} />
+          </span>
         </section>
       </div>
     </section>
