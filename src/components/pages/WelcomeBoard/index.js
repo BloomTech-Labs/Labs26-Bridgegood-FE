58672b12070e0c1c21bf0466b1f93da12aa04d0b
Ana Carrillo <acarrillo9724@gmail.com>
2020-09-21 18:55:05 -0400

    created a component for the welcome dashboard

---
+++ src/components/pages/WelcomeBoard/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WelcomeBoard () {
+    return (
+        <>
+            <p>WELCOME TO THE FAMILY!</p>
+            <button>Reserve a studio space now</button>
+        </>
+    );
+};
+
+export default WelcomeBoard;
