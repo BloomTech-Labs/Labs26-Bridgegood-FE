e957ba130c63a2a1b1393d3afb8f15ac0ef76975
Yasir Hassan <hasanyasir01@gmail.com>
2020-09-23 11:32:18 -0700

    linked login btn to /login endpoint

--- src/components/pages/Home/Landing/Header/Btns/Buttons.js
+++ src/components/pages/Home/Landing/Header/Btns/Buttons.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import classes from './Buttons.module.css';
+import { Link } from 'react-router-dom';
 const Buttons = props => {
   return (
     <div className={classes.Buttons}>
-      <button className={classes.btn_login}>log in</button>
-      <button className={classes.btn_donate}>donate</button>
+      <Link to="/login">
+        <button className={classes.btn_login}>log in</button>
+      </Link>
+      <Link>
+        <button className={classes.btn_donate}>donate</button>
+      </Link>
     </div>
   );
 };
