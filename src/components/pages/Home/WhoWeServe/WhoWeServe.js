0b621d43e52561e55cd80d1eb2b85db01f764142
Yasir Hassan <hasanyasir01@gmail.com>
2020-09-29 14:37:52 -0700

    who we serve section and some modification

--- src/components/pages/Home/WhoWeServe/WhoWeServe.js
+++ src/components/pages/Home/WhoWeServe/WhoWeServe.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import classes from './WhoWeServe.module.css';
+import './WhoWeServe.less';
 import CreativesImg from '../../../../assets/images/creatives.png';
 import FrameImg from '../../../../assets/images/Frame.png';
 import SchoolImgLoc from '../../../../assets/images/schoolLocation.png';
 
 const WhoWeServer = props => {
   return (
-    <section className={classes.WhoWeServe}>
+    <section className="WhoWeServe">
       <p>WHO WE SERVE</p>
-      <div className={classes.SubWhoWeServe}>
+      <div className="SubWhoWeServe">
         <div>
           <img src={SchoolImgLoc} alt="Creatives Img" />
-          <h2>Schools in Northern California</h2>
+          <h3>Schools in Northern California</h3>
         </div>
         <div>
           <img src={FrameImg} alt="Creatives Img" />
-          <h2>Black, Indigenous, People of Color</h2>
+          <h3>Black, Indigenous, People of Color</h3>
         </div>
         <div>
           <img src={CreativesImg} alt="Creatives Img" />
-          <h2>BRIDGEGOOD Creatives</h2>
+          <h3>BRIDGEGOOD Creatives</h3>
         </div>
       </div>
     </section>
