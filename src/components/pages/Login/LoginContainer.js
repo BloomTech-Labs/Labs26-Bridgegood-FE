a3456674e846b01f9fb25304aa8003a1e7de9154
Anthony K <antonyk@users.noreply.github.com>
2020-10-15 17:16:58 -0400

    cleanup

--- src/components/pages/Login/LoginContainer.js
+++ src/components/pages/Login/LoginContainer.js
@@ -8,57 +8,16 @@ import SignUp from '../../common/SignUp';
 import Login from '../../common/Login';
 import './Login.less';
 
-const LoginContainer = () => {
-  useEffect(() => {
-    const { pkce, issuer, clientId, redirectUri, scopes } = config;
-    // destructure your config so that you can pass it into the required fields in your widget.
-    const widget = new OktaSignIn({
-      baseUrl: issuer ? issuer.split('/oauth2')[0] : '',
-      clientId,
-      redirectUri,
-      // takes a few different hooks, the config options
-      registration: {
-        // there is more we can do to handle some errors here.
-      },
-      features: { registration: false },
-      // turning this feature on allows your widget to use Okta for user registration
-      logo: img,
-      // add your custom logo to your signing/register widget here.
-      i18n: {
-        en: {
-          'primaryauth.title': 'LOG IN',
-          // change title for your app
-        },
-      },
-      authParams: {
-        pkce,
-        issuer,
-        display: 'page',
-        scopes,
-      },
-    });
-
-    // widget.renderEl(
-    //   { el: '#sign-in-widget' },
-    //   () => {
-    //     /**
-    //      * In this flow, the success handler will not be called because we redirect
-    //      * to the Okta org for the authentication workflow.
-    //      */
-    //   },
-    //   err => {
-    //     throw err;
-    //   }
-    // );
-  }, []);
+const LoginContainer = ({ authType }) => {
+  const type = authType ? authType : 'login';
 
   return (
     <div className="signUpBox">
       <section>
-        <Login enabled={true} />
+        <Login enabled={type === 'login'} />
       </section>
       <section>
-        <SignUp enabled={false} />
+        <SignUp enabled={type === 'signup'} />
       </section>
       {/* <section className="main-container">
         <div className="welcome-text"></div>
